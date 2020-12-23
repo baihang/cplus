1da738271d1fb4dc1ae1250ee2ed422418a19960
baihang <1448863597@qq.com>
2020-12-23 23:29:09 +0800

    Vector

--- hang/Vector.cpp
+++ hang/Vector.cpp
@@ -1,36 +1,28 @@
-/**
- * 参考 邓俊辉教授所著的《数据结构》
- * */
-typedef int Rank;
-#define DEFAULT_CAPACITY 3
+#include "Vector"
 
-namespace hang
+class Vector
 {
-
+private:
+    /* data */
     template <typename T>
-    class Vector
+    void Vector::copeFrom(T const *A, Rank lo, Rank hi)
     {
-    protected:
-        Rank _size;
-        int _capacity;
-        T *_elem;
-
-        void copyFrom(T *A, Rank lo, Rank hi);
-        void expand();                 //扩容
-        void shrink();                 //缩容
-        bool bobble(Rank lo, Rank hi); //扫描交换
-        void bubbleSort(Rank lo, Rank hi);
-        Rank max(Rank lo, Rank hi);
-        void selectionSort(Rank lo, Rank hi);
-        void merge(Rank lo, Rank mi, Rank hi);
-        void mergeSort(Rank lo, Rank hi);
-        void partition(Rank lo, Rank hi); //轴点构造算法
-        void quickSort(Rank lo, Rank hi);
-        void heapSort(Rank lo, Rank hi);
-
-        int main(){
-            return 0;
+        _elem = new T[_capacity = 2 * (hi - lo)];
+        _size = 0;
+        while(lo < hi){
+            _elem[_size] = A[lo++];
         }
-    };
+    }
 
-} // namespace hang
+public:
+    Vector(/* args */);
+    ~Vector();
+};
+
+Vector::Vector(/* args */)
+{
+}
+
+Vector::~Vector()
+{
+}
