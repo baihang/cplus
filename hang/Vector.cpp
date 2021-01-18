9ff0868f52fbd274b6df39c585434f85c1c40ec9
baihang <1448863597@qq.com>
2021-01-18 22:29:07 +0800

    vector

--- hang/Vector.cpp
+++ hang/Vector.cpp
@@ -1,16 +1,78 @@
 #include "Vector"
 
+template <typename T>
 class Vector
 {
 private:
     /* data */
     template <typename T>
-    void Vector::copeFrom(T const *A, Rank lo, Rank hi)
+    void Vector<T>::copeFrom(T const *A, Rank lo, Rank hi)
     {
         _elem = new T[_capacity = 2 * (hi - lo)];
         _size = 0;
-        while(lo < hi){
-            _elem[_size] = A[lo++];
+        while (lo < hi)
+        {
+            _elem[_size++] = A[lo++];
+        }
+    }
+
+    Vector<int> array = new Vector();
+
+    template <typename T>
+    Vector<T> &Vector<T>::operator=(Vector<T> const &v)
+    {
+        if (_elem)
+            delete _elem;
+        copeFrom(v._elem, 0, v.size());
+        return *this;
+    }
+
+    template <typename T>
+    T &Vector::operator[](Rank r)
+    {
+        return _elem[r];
+    }
+
+    /**
+ * 分摊时间复杂度 O(1)
+ * 
+ * 装填因子  _size / _capacity
+ * 
+ * */
+    template <typename T>
+    void Vector<T>::expand()
+    {
+        if (_size < _capacity)
+            return;
+        T *old = _elem;
+        _elem = new T[_capacity <<= 1];
+        for (int i = 0; i < _size; i++)
+        {
+            _elem[i] = old[i];
+        }
+        delete[] old;
+    }
+
+    template <typename T>
+    void Vector<T>::shrink()
+    {
+        if (_capacity < DEFAULT_CAPACITY << 1)
+            return;
+        if (_size << 1 > _capacity) //25%
+            return;
+
+        T *old = _elem;
+        _elem = new T[_capacity >>= 1];
+        for (int i = 0; i < _size; i++)
+        {
+            _elem[i] = ole[i];
+        }
+        delete[] old;
+    }
+
+    template <typename T> void &Vector<T>::permute(Vector<T> v){
+        for(int i = v.size(); i > 0; i--){
+            swap(v[i - 1], v[rand() % i])
         }
     }
 
@@ -19,10 +81,12 @@ public:
     ~Vector();
 };
 
-Vector::Vector(/* args */)
+template <typename T>
+Vector<T>::Vector(/* args */)
 {
 }
 
-Vector::~Vector()
+template <typename T>
+Vector<T>::~Vector()
 {
 }
